b8834fa780b15ba5fcb9d813f369f935a1f2d4a9
sanket <sanket.shivam467@gmail.com>
2019-05-16 23:50:32 +0530

    Trying to register

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,59 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+//Load user model
+
+const User = require('../../models/Users');
 
 //@route  GET api/users/test
 //@desc   Tests user route
 //@access Public
 
 router.get('/test', 
-  (req, res) => res.json({msg: 'User Works'}));
+  (req, res) => res.json({msg: "users works"}));
+
+//@route  GET api/users/register
+//@desc   Register User
+//@access Public
+
+router.post('/register',(req, res)=>{
+ User.findOne({
+     email:req.body.email
+ }).then(user => {
+     if(user){
+         return res.status(400).json({email: 'Email already exists'});
+     }
+     else{
+         const avatar = gravatar.url(req.body.email ,
+            {
+                s:'200',
+                r:'pg',
+                d:'mm'
+            });
+         const newUser = new User({
+             name: req.body.name,
+            email: req.body.email,
+            avatar,
+            password: req.body.email
+         });
+
+         bcrypt.genSalt(10, (err, salt) => {
+             bcrypt.hash(newUser.password, salt, (err, hash) => {
+                 if(err) throw err;
+                 newUser.password = hash;
+                 newUser.save()
+                   .then(user => res.json(user))
+                   .catch(err => console.log(err));
+             })
+         })
+
+
+     }
+ });
+}
+)
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profiles = require('./routes/api/profiles');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+//Bodyparser middle ware
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //DB Config 
 const db =  require('./config/keys').mongoURI;
@@ -20,9 +26,9 @@ app.get('/', (req, res)=> res.send('Hello World!'));
 
 //Use Routes
 
-app.use('./api/users', users);
-app.use('./api/profiles', profiles);
-app.use('./api/posts', posts);
+app.use('/api/users', users);
+app.use('/api/profiles', profiles);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 

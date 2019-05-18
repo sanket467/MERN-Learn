4b943bbef30be64660f35b16cdac194c54f07aeb
sanket <sanket.shivam467@gmail.com>
2019-05-18 17:21:34 +0530

    Validation Applied on user

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,11 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+//Load Input Validation
+
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
 //Load user model
 
 const User = require('../../models/Users');
@@ -22,11 +27,20 @@ router.get('/test',
 //@access Public
 
 router.post('/register',(req, res)=>{
+   
+  const {errors, isValid} = validateRegisterInput(req.body);
+
+  //Check Validation
+
+  if(!isValid){
+      return res.status(400).json(errors);
+  }
  User.findOne({
      email:req.body.email
  }).then(user => {
      if(user){
-         return res.status(400).json({email: 'Email already exists'});
+         errors.email = "Email already exists";
+         return res.status(400).json(errors);
      }
      else{
          const avatar = gravatar.url(req.body.email ,
@@ -64,14 +78,24 @@ router.post('/register',(req, res)=>{
 //@access Public
 
 router.post('/login', (req, res) => {
+
+    const {errors, isValid} = validateLoginInput(req.body);
+
+  //Check Validation
+
+  if(!isValid){
+      return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   //Find user by email
   User.findOne({email:email}).then(user => {
   //Check for user
+  errors.email = 'User not found';
   if(!user){
-      return res.status(404).json({email: "User not found"});
+      return res.status(404).json(errors);
   }
 
   //Check Password
@@ -99,7 +123,8 @@ router.post('/login', (req, res) => {
 
 
       }else{
-          return res.status(400).json({password: 'Password Incorrect'});
+          errors.password = 'Password Incorrect';
+          return res.status(400).json(errors);
       }
   });
   });

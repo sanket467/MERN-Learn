e7851aabdb10e0dfcc7e8d13cc152cf5816f4e53
sanket <sanket.shivam467@gmail.com>
2019-05-26 11:35:47 +0530

    Landing Page completed L-33

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/layouts/Navbar";
 import Footer from "./Components/layouts/Footer";
-import Landing from './Components/layouts/Landing';
+import Landing from "./Components/layouts/Landing";
+import Login from "./Components/auth/Login";
+import Register from "./Components/auth/Register";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing/>
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
